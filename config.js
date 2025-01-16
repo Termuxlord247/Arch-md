4e4d8bf9e66208abd34f492223d4563e1385db99
Termuxlord247 <nikkysmart448@gmail.com>
2025-01-16 15:47:13 +0100

    config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
+global.owner = ["2349061869218"]
 global.botname = ["Arch Md"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
