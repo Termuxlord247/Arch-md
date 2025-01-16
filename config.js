f38ec1bb18f31e1b50878ffc81b0d7ed0f3dcff7
Termuxlord247 <nikkysmart448@gmail.com>
2025-01-16 15:35:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,8 +8,8 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
-global.botname = ["Arch Md"]
+global.owner = ["2349061869218"]
+global.botname = ["CHUCKY Md"]
 global.simbol = "♘"
 global.typeMenu = 'v5' //don't change 
 global.anticall = false // set true to turn anticall always on
